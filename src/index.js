b6e849af9b905e3fdc45650b853b1e045cfb041e
Ramtin Khoee <ramtin.khoee@gmail.com>
2021-03-01 15:03:24 -0800

    test branch

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { HashRouter } from 'react-router-dom';
+import CodeProvider from './provider/CodeProvider.tsx';
 import App from './App';
 
 ReactDom.render(
   // providing hashrouter for electron static application
   // need to add providers here v
   <HashRouter>
+    <CodeProvider>
         <App />
+    </CodeProvider>
   </HashRouter>,
   document.getElementById('root'),
 );
