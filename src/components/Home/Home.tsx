7e1d184f2da2c3ab7f29fa7aab428b16431e9431
Ramtin Khoee <ramtin.khoee@gmail.com>
2021-03-02 18:14:17 -0800

    file tree sorter proto

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,6 +2,7 @@
 /* eslint-disable import/no-unresolved */ // ! Be careful
 
 import React, { useContext } from 'react';
+import { render } from 'react-dom';
 import { InputState } from '../../provider/CodeProvider';
 // const fs = require ('fs');
 
@@ -10,12 +11,13 @@ const { remote } = window.require('electron');
 const fs = remote.require('fs')
 let current: string = '';
 let readDir;
+let fileTree = [];
 
 
 const Home = () => {
     // destructuring state/useState methods from Context
     const { fileTreeHandler, pathHandler }: any = useContext(InputState);
-    const fileTree = () => {
+    const fileTreeCreator = () => {
     const getFilePath = () => {
         remote.dialog
         .showOpenDialog({properties: ['openDirectory'],
@@ -23,22 +25,28 @@ const Home = () => {
         .then((files: any) => {
             if (!files.cancelled) {
             current = files.filePaths[0];
-            getDirectory()
+            getDirectory(current)
             }
         })
     }
     getFilePath()
-    let getDirectory = () => {
-        const filterArray = fs.readdirSync(current)
+    let getDirectory = (current) => {
+        const filterArray = fs.readdirSync(current).filter(file => file !== 'node_modules' && file[0] !== '.');
+        fileTree = filterArray
         console.log('filepath', current)
         console.log('array of files', filterArray)
+        fileSorter()
         }
+
+    let fileSorter = () => {
+        console.log(fileTree.filter(file => !file.includes('.'))
+    }
+    
 }
+
     return (
         <div>
-            {/* <button onClick={getFilePath}>file</button>
-            <button onClick={readDirectory}>read</button> */}
-            <button onClick={fileTree}>test</button>
+            <button onClick={fileTreeCreator}>test</button>
         </div>
     )
 
